2bf03950a0b1ea87f161191b7c687779eb6dc354
Axel Froborg <axel.froborg@gmail.com>
2021-12-30 18:28:34 +0100

    Fix docker compose

--- pkg/utils/env.util.go
+++ pkg/utils/env.util.go
@@ -2,17 +2,9 @@ package utils
 
 import (
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnvVariable(key string, fallback string) string {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		return fallback
-	}
-
 	val := os.Getenv(key)
 
 	if len(val) == 0 {
