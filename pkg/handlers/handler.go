5b618edc6ec483cf5db50737e92e659cb143fbdd
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-01-01 20:26:41 +0100

    fix content tyep

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -16,7 +16,7 @@ func New(db *gorm.DB) handler {
 }
 
 func (h handler) respond(w http.ResponseWriter, data interface{}, statusCode int) {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
